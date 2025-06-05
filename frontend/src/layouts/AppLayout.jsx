c69d4eda469515c4cfc4c27b90c76c22b73fb20a
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-05 15:55:27 +0200

    WIP(UI/UX): the hero is under construction

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -5,7 +5,7 @@ export default function AppLayout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-1 p-4 bg-red-500">
+      <main className="flex-1 bg-gray-600">
         <Outlet />
       </main>
     </div>
