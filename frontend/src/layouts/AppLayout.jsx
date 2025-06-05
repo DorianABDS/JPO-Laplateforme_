695e1a81125dc20a6fab063c3597510a4cc1a146
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-05 21:09:41 +0200

    feat(components\Footer): add Footer component and integrate logo in the layout

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function AppLayout() {
   return (
@@ -11,6 +12,10 @@ export default function AppLayout() {
       <main className="bg-gray-400 flex-1 overflow-auto">
         <Outlet />
       </main>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
