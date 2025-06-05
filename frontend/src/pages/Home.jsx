794b6d16161dc3c220063bd726381cda5f831648
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-05 15:55:27 +0200

    WIP(UI/UX): the hero is under construction

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import CardJPO from "../components/CardJPO";
 
 export default function Home() {
-    const {jpos, setJpos} =  useState([])
+  const [jpos, setJpos] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
     fetch(`${import.meta.env.VITE_API_URL}/api/jpo.php`)
       .then((res) => res.json())
       .then((data) => {
@@ -16,10 +16,27 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {jpos.map((open_day) => (
-        <CardJPO key={open_day.id} jpo={open_day} />
-      ))}
+    <div>
+      {/* Hero Section */}
+      <section
+        className="relative bg-cover bg-center h-screen p-0 flex flex-col justify-center items-center text-white"
+        style={{ backgroundImage: "url('../src/assets/img/bg-hero.svg')" }}
+      >
+        <h1 className="text-4xl font-bold mb-2">
+          Bienvenue aux Journées Portes Ouvertes
+        </h1>
+        <p className="max-w-xl text-center">
+          Découvrez nos campus, rencontrez les enseignants et explorez nos
+          formations.
+        </p>
+      </section>
+
+      {/* Liste des JPO */}
+      <section className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {jpos.map((open_day) => (
+          <CardJPO key={open_day.id} jpo={open_day} />
+        ))}
+      </section>
     </div>
   );
 }
