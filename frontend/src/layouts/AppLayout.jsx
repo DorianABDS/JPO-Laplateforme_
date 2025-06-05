62b1e835994e2de86ad16b4bab7956dc74f2b542
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-05 19:57:52 +0200

    style(layouts\AppLayout): reorganize structure and improve layout semantics with header element

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -1,13 +1,16 @@
-import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function AppLayout() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-1 bg-gray-600">
+    <div className="min-h-screen flex flex-col">
+      <header>
+        <Header />
+      </header>
+
+      <main className="flex-1 overflow-auto">
         <Outlet />
       </main>
     </div>
   );
-};
+}
