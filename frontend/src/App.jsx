789774f1368733bb14cdc02474509a4f5337d303
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-05 23:56:03 +0200

    feat(App): add admin routing and components for admin dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import Events from "./pages/Events";
+import AdminLayout from "./layouts/AdminLayout";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AdminUsers from "./pages/admin/AdminUsers";
+import AdminJPO from "./pages/admin/AdminJPO";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
-import './App.css';
-import { Children } from "react";
 
 const router = createBrowserRouter([
   {
@@ -15,7 +17,6 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: "", element: <Home /> },
-      // { path: "jpo/:id", element: <JpoDetail /> },
       { path: "profile", element: <Profile /> },
       { path: "events", element: <Events /> },
       { path: "login", element: <Login /> },
@@ -23,7 +24,18 @@ const router = createBrowserRouter([
       { path: "*", element: <NotFound /> },
     ],
   },
+
+  {
+    path: "/adminboard",
+    element: <AdminLayout />,
+    children: [
+      { index: true, element: <AdminDashboard /> },
+      { path: "users", element: <AdminUsers /> },
+      { path: "jpo", element: <AdminJPO /> },
+    ],
+  },
 ]);
+
 
 export default function App() {
   return <RouterProvider router={router} />;
