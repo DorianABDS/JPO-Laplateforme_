a5ae5dca3f6c5f3479bebedd32348a98fffe75a6
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-06 10:48:48 +0200

    style(pages/admin): update heading colors for improved visibility

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,3 +1,3 @@
 export default function AdminDashboard() {
-  return <h1 className="text-2xl font-bold">Dashboard</h1>;
+  return <h1 className="text-2xl text-[#0062FF] font-bold">Dashboard</h1>;
 }
