d164ffdfd187ad81d93748876acb960ab8b0a202
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-06 15:09:12 +0200

    refactor(components/Button): enhance Button component with customizable props

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,14 +1,17 @@
 import { useNavigate } from "react-router-dom";
 
-export default function Button() {
+export default function Button({
+  to = "/events",
+  label = "S'inscrire",
+  className = ""
+}) {
   const navigate = useNavigate();
 
+  const baseClass = "text-md bg-white text-[#0062F0] px-5 py-2 rounded-full hover:bg-[#d6d6d6] sm:text-2xl font-semibold text-white h-14 sm:h-14 w-full sm:w-52 rounded-full transition"
+
   return (
-    <button
-      onClick={() => navigate("/events")}
-      className="text-xl sm:text-2xl font-semibold my-5 bg-[#0062FF] hover:bg-[#0051cc] h-14 sm:h-16 w-full sm:w-64 rounded-full transition"
-    >
-      Explorer nos JPO
+    <button onClick={() => navigate(to)} className={`${baseClass} ${className}`}>
+      {label}
     </button>
   );
 }
