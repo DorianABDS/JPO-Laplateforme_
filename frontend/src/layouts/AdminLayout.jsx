659293f9d2bac68fdf0ef42c1c642e5c8ae9f1c8
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-06 19:51:56 +0200

    style(layouts/AdminLayout): restructure sidebar links into a list for improved organization

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -7,16 +7,34 @@ export default function AdminLayout() {
       {/* Sidebar */}
       <aside className="w-64 bg-[#0062FF] text-white p-6 flex flex-col gap-4">
         <h2 className="text-2xl font-bold mb-6">Admin</h2>
-        <Link to="/admindashboard" className="flex gap-3 hover:text-gray-400">
-          <House /> Dashboard
-        </Link>
-        <Link to="/admindashboard/users" className="flex ml-3 gap-3 hover:text-gray-400">
-          <User /> Utilisateurs
-        </Link>
-        <Link to="/admindashboard/jpo" className="flex ml-3 gap-3 hover:text-gray-400">
-          <DoorOpen /> JPO
-        </Link>
-        <Link to="/" className="flex gap-3 mt-auto text-sm text-gray-300 hover:text-white">
+
+        <ul>
+          <li className="mb-4">
+            <Link to="/admindashboard" className="flex gap-3 hover:text-gray-400">
+              <House /> Dashboard
+            </Link>
+          </li>
+
+          {/* Conteneur avec bordure gauche continue */}
+          <div className="ml-3 border-l-2 border-white pl-3 space-y-4">
+            <li>
+              <Link to="/admindashboard/users" className="flex gap-3 hover:text-gray-400">
+                <User /> Utilisateurs
+              </Link>
+            </li>
+
+            <li>
+              <Link to="/admindashboard/jpo" className="flex gap-3 hover:text-gray-400">
+                <DoorOpen /> JPO
+              </Link>
+            </li>
+          </div>
+        </ul>
+
+        <Link
+          to="/"
+          className="flex gap-3 mt-auto text-sm text-gray-300 hover:text-white"
+        >
           <CornerDownLeft /> Retour au site
         </Link>
       </aside>
