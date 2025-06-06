48339eb52034d3fe7c15e2a47ef2d245116a95a8
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-07 01:17:38 +0200

    refactor(Home): improve layout and formatting of sections

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -27,23 +27,59 @@ export default function Home() {
           minHeight: "calc(100vh - 4rem)",
         }}
       >
-        <h1 className="text-4xl sm:text-7xl md:text-8xl lg:text-9xl font-trench mb-2">LaPlateforme</h1>
+        <h1 className="text-4xl sm:text-7xl md:text-8xl lg:text-9xl font-trench mb-2">
+          LaPlateforme
+        </h1>
         <p className="max-w-2xl text-base sm:text-lg md:text-xl font-Poppins mb-6">
-          Découvrez nos campus grâce à nos journée porte ouverte, rencontrez nos enseignants et explorez nos formations.
+          Découvrez nos campus grâce à nos journée porte ouverte, rencontrez nos
+          enseignants et explorez nos formations.
         </p>
-        <ButtonHero
-          label="Découvrir"
-          to="/events"
-          className=""
+        <ButtonHero label="Découvrir" to="/events" className="" />
+      </section>
+
+      <section>
+        <p className="font-trench text-4xl font-bold text-[#0062FF] mt-20 mb-20 flex justify-center">
+          Journée porte ouverte à venir
+        </p>
+        <CardJPO
+          jpo={{
+            name: "Martigues - Journée porte ouverte",
+            date: "2025-06-16",
+            max_capacity: 100,
+            campus_id: "Martigues",
+          }}
         />
       </section>
 
-      {/* Liste des JPO */}
+      <section>
+        <CardJPO
+          jpo={{
+            name: "Martigues - Journée porte ouverte",
+            date: "2025-06-17",
+            max_capacity: 50,
+            campus_id: "Martigues",
+          }}
+        />
+      </section>
+      
+      <section>
+        <CardJPO
+          jpo={{
+            name: "Martigues - Journée porte ouverte",
+            date: "2025-06-17",
+            max_capacity: 50,
+            campus_id: "Martigues",
+          }}
+        />
+      </section>
+
+
+      {/* Liste des JPO
       <section className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
         {jpos.map((open_day) => (
           <CardJPO key={open_day.id} jpo={open_day} />
         ))}
-      </section>
+      </section> */}
     </div>
   );
 }
