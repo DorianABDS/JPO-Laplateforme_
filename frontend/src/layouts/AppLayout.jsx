82ebf7d5a1dc72eac2a625833b87bb82cf7cbc76
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-07 01:14:30 +0200

    feat(AppLayout): implement dynamic document title based on current route

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -1,15 +1,34 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useEffect } from "react";
+
+const titlesByPath = {
+  "/": "Accueil - JPO-Laplateforme_",
+  "/profil": "Profil - JPO-Laplateforme_",
+  "/evenement": "Événements - JPO-Laplateforme_",
+  "/connexion": "Connexion - JPO-Laplateforme_",
+  "/inscription": "Inscription - JPO-Laplateforme_",
+  "/admindashboard": "Admin - JPO-Laplateforme_",
+  "/admindashboard/users": "Admin - Utilisateurs - JPO-Laplateforme_",
+  "/admindashboard/jpo": "Admin - JPO - JPO-Laplateforme_",
+};
 
 export default function AppLayout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const title = titlesByPath[location.pathname] || "JPO-Laplateforme_";
+    document.title = title;
+  }, [location.pathname]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <header>
         <Header />
       </header>
 
-      <main className="bg-gray-400 flex-1 overflow-auto">
+      <main className="bg-white mt-[64px] flex-1 overflow-auto">
         <Outlet />
       </main>
 
