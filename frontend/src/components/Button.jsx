496b7ba45b70f76e296367b77adf44fa11ab6a5b
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-07 16:13:21 +0200

    refactor(Button): update baseClass to include hover effect for better UI feedback

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -7,7 +7,7 @@ export default function Button({
 }) {
   const navigate = useNavigate();
 
-  const baseClass = "mt-4 bg-white text-[#0062FF] px-6 py-2 rounded-full font-semibold hover:bg-gray-100 transition duration-200 shadow self-start"
+  const baseClass = "mt-4 bg-white text-[#0062FF] hover:bg-gray-300 px-6 py-2 rounded-full font-semibold hover:bg-gray-100 transition duration-200 shadow self-start"
 
   return (
     <button onClick={() => navigate(to)} className={`${baseClass} ${className}`}>
