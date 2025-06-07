ac20afa4fa3b7f094f48539f59d3f3a2fb037e49
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-07 19:32:00 +0200

    fix(CallToAction): adjust layout and styling for improved responsiveness

--- frontend/src/components/CallToAction.jsx
+++ frontend/src/components/CallToAction.jsx
@@ -3,12 +3,12 @@ import HeroButton from "./HeroButton";
 export function CallToAction() {
   return (
     <div className="flex justify-center items-center mt-16 mb-20">
-      <div className="h-44 w-[50%] bg-[#0062FF] flex flex-col justify-center items-center rounded-xl gap-5">
+      <div className="h-48 sm:h-44 w-[90%] bg-[#0062FF] text-center px-8 flex flex-col justify-center items-center rounded-xl gap-5">
         <p className="font-Poppins text-lg font-medium">
           Envie d’en voir plus ? Nos prochains événements n’attendent que vous.</p>
         <HeroButton
           to="/events"
-          className="bg-white !text-[#0062FD] hover:bg-gray-300 !w-72" 
+          className=" bg-white !text-[#0062FD] hover:bg-gray-300 md:!w-72" 
         />
       </div>
     </div>
