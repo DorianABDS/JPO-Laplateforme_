e2091e6922bd9f8fcab3403509b5338c91986633
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2025-06-08 04:56:24 +0200

    fix(CallToAction): improve code formatting and maintain consistency in JSX structure

--- frontend/src/components/CallToAction.jsx
+++ frontend/src/components/CallToAction.jsx
@@ -2,17 +2,30 @@ import { Button } from "./Button";
 
 export function CallToAction() {
   return (
-    <div className="flex justify-center items-center mt-16 mb-20 ">
-      <div className="h-48 sm:h-44 w-[90%] bg-gradient-to-br 
-                    from-[#0062FF] via-[#0052CC] to-[#0041AA] text-center px-8 flex flex-col justify-center items-center rounded-xl gap-5">
+    <div className="flex justify-center items-center mt-16 mb-20">
+      {/* Container */}
+      <div
+        className="
+          h-48 sm:h-44 
+          w-[90%] 
+          bg-gradient-to-br from-[#0062FF] via-[#0052CC] to-[#0041AA] 
+          text-center px-8 
+          flex flex-col justify-center items-center rounded-xl gap-5
+        "
+      >
+        {/* Texte descriptif */}
         <p className="font-Poppins text-lg font-medium">
-          Envie d’en voir plus ? Nos prochains événements n’attendent que vous.</p>
+          Envie d’en voir plus ? Nos prochains événements n’attendent que vous.
+        </p>
+
+        {/* Bouton */}
         <Button
           to="/events"
-          label = "Explorer nos JPO"
-          className="text-lg sm:text-2xl font-semibold h-14 md:!w-72" 
+          label="Explorer nos JPO"
+          size=""
+          className="font-semibold h-14 md:!w-72"
         />
       </div>
     </div>
-  )
+  );
 }
